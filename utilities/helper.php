b95c295ff2b19a577cf986303d8f02f2d63e2f63
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-04-29 10:06:08 +0430

    Add isConnectedToTelegram function to check user connection status

--- utilities/helper.php
+++ utilities/helper.php
@@ -1,2 +1,11 @@
 <?php
 
+function isConnectedToTelegram()
+{
+    $sql = "SELECT * FROM telegram WHERE user_id = :user_id";
+    $stmt = DB->prepare($sql);
+    $stmt->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return !empty($result);
+}
