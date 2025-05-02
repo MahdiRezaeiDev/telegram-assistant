19024d0cc4516266c8b5e72dcaa2497f3e6149ff
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-05-02 10:04:45 +0430

    Update binary file safe.php in telegram session directory

---
+++ app/controller/telegram/ContactsController.php
@@ -0,0 +1,15 @@
+<?php
+if (!isset($DB_NAME)) {
+    // If the constant is not defined, it means this file is being accessed directly.
+    header("Location: ../../../views/auth/403.php");
+}
+
+
+$contacts = getContacts(USER_ID);
+
+function getContacts($userId)
+{
+    $stmt = DB->prepare("SELECT * FROM contacts WHERE user_id = ?");
+    $stmt->execute([$userId]);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
