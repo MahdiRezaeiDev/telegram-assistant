0d929c2674c937efe744a46f94556144000b0ff4
MahdiRezaeiDev <mahdi.sohaily4030@gmail.com>
2025-05-05 19:10:37 +0430

    refactor(constants): remove unused group and chat limit constants

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,5 @@
 <?php
 session_start();
-// The group or chat identifier (it could be user name, ID, invitation link, etc).
-define('GROPE_LINK', '/t.me/+P83Wt7C1421iZDU0');
-define('CHAT_LIMIT', 50);
-
 $HOST = 'localhost';
 $DB_NAME = 'cheraghbargh';
 $USERNAME = 'root';
